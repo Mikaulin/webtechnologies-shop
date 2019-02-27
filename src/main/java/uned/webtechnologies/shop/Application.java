ac11829a5a52845f8a6405f5c284d8c7b5041d49
Mikaulin <mikausin@gmail.com>
2019-02-27 17:58:44 +0100

    Configuration changes for running in Java 1.7.

--- src/main/java/uned/webtechnologies/shop/Application.java
+++ src/main/java/uned/webtechnologies/shop/Application.java
@@ -3,7 +3,8 @@ package uned.webtechnologies.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
