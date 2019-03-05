41a037a6ce31c6a843d5a7a433bc25f379078ebf
Mikaulin <mikausin@gmail.com>
2019-03-05 21:03:23 +0100

    Basic authentication using spring security.

--- src/main/java/uned/webtechnologies/shop/inmemorydb/model/User.java
+++ src/main/java/uned/webtechnologies/shop/inmemorydb/model/User.java
@@ -2,15 +2,27 @@ package uned.webtechnologies.shop.inmemorydb.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Transient;
+import java.util.Set;
 
 @Entity
 public class User {
 
     @Id
     private Long id;
+
     private String username;
+
     private String password;
+
+    @Transient
+    private String passwordConfirm;
+
     private int age;
+
+    @ManyToMany
+    private Set<Role> roles;
 
     public User() {
     }
@@ -22,7 +34,53 @@ public class User {
         this.age = age;
     }
 
-    //standard setters and getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     @Override
     public String toString() {
