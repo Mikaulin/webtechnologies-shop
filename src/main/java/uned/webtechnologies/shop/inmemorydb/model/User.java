d34cf13b46972f9072550b0d3e45c667cb37ac9e
Pablo <puzquiano3@gmail.com>
2019-03-18 23:17:34 +0100

    Cambios comentados con Mikel.
    phone.
    postCode
    BrandRepositoty

--- src/main/java/uned/webtechnologies/shop/inmemorydb/model/User.java
+++ src/main/java/uned/webtechnologies/shop/inmemorydb/model/User.java
@@ -20,8 +20,8 @@ public class User {
     private String province;
     private String country;
     private String email;
-    private String tlf;
-    private int pc;
+    private String phone;
+    private int postalCode;
     private String password;
 
     @Transient
@@ -51,6 +51,109 @@ public class User {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname1() {
+        return lastname1;
+    }
+
+    public void setLastname1(String lastname1) {
+        this.lastname1 = lastname1;
+    }
+
+    public String getLastname2() {
+        return lastname2;
+    }
+
+    public void setLastname2(String lastname2) {
+        this.lastname2 = lastname2;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setTlf(String phone) {
+        this.phone = phone;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public boolean isSubscribed() {
+        return subscribed;
+    }
+
+    public void setSubscribed(boolean subscribed) {
+        this.subscribed = subscribed;
+    }
+
+    public Set<Cart> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(Set<Cart> carts) {
+        this.carts = carts;
+    }
+
+    public Set<PurchaseLine> getPurchaseLines() {
+        return purchaseLines;
+    }
+
+    public void setPurchaseLines(Set<PurchaseLine> purchaseLines) {
+        this.purchaseLines = purchaseLines;
+    }
 
     public long getId() {
         return id;
@@ -100,9 +203,23 @@ public class User {
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", lastname1='" + lastname1 + '\'' +
+                ", lastname2='" + lastname2 + '\'' +
+                ", direction='" + direction + '\'' +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", country='" + country + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", postalCode=" + postalCode +
                 ", password='" + password + '\'' +
-                ", age=" +
+                ", passwordConfirm='" + passwordConfirm + '\'' +
+                ", username='" + username + '\'' +
+                ", subscribed=" + subscribed +
+                ", carts=" + carts +
+                ", purchaseLines=" + purchaseLines +
+                ", roles=" + roles +
                 '}';
     }
 }
