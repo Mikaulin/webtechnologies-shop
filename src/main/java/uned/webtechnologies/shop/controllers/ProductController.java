6463888b466f6e7e403d317d20530e25736ad6c8
Pablo <puzquiano3@gmail.com>
2019-05-07 06:12:30 +0200

    Comentados con javadoc ProductController

--- src/main/java/uned/webtechnologies/shop/controllers/ProductController.java
+++ src/main/java/uned/webtechnologies/shop/controllers/ProductController.java
@@ -34,9 +34,13 @@ public class ProductController {
     }
 
 
-    /**Método
-     * @param id
-     * @return
+    /**Método que responde a la solicitud GET ("/producto/detalle/{id}") poniendo a disposición de la vista
+     * un producto, donde {id} es el identificador único del producto que se requiere
+     * @param id Identificador único del producto
+     * @return ModelAndView ("product/detail") con el producto que se solicita
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/ModelAndView.html">ModelAndView</a>
+     * @see ProductService#getProduct(long)
+     *
      */
     @GetMapping("/detalle/{id}")
     public ModelAndView detail(@PathVariable("id") long id) {
@@ -45,12 +49,27 @@ public class ProductController {
         return result;
     }
 
+    /**Método que responde a la solicitud GET ("/producto/valoraciones") redireccionando a la
+     * correspondiente vista
+     *
+     * @return ModelAndView ("search/rating")
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/ModelAndView.html">ModelAndView</a>
+     *
+     */
     @GetMapping("/valoraciones")
     public ModelAndView rating() {
         ModelAndView result = new ModelAndView("search/rating");
         return result;
     }
 
+    /**Método que responde a la solicitud GET ("producto/valoraciones/[id}")
+     * poniendo a disposición de la vista ("serach/list") los productos que tienen una valoración concreta
+     * donden {id} es la valoración solicitada
+     * @param id Identificador único de la valoración
+     * @return ModelAndView ("search/list") con la lista de poductos que tienen la valoración solicitada
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/ModelAndView.html">ModelAndView</a>
+     * @see RatingService#getProductRating(long)
+     */
     @GetMapping("/valoraciones/{id}")
     public ModelAndView ratinglist(@PathVariable("id") int id) {
         ModelAndView result = new ModelAndView("search/list");
