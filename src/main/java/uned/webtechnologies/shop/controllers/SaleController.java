d1693259828a1ce96204a5b1d2cb3344b1ec55f5
Pablo <puzquiano3@gmail.com>
2019-05-08 06:32:36 +0200

    SaleController: Eliminado metodo de informes que solo se utiliza en SaleAdminController.

--- src/main/java/uned/webtechnologies/shop/controllers/SaleController.java
+++ src/main/java/uned/webtechnologies/shop/controllers/SaleController.java
@@ -17,6 +17,12 @@ import javax.persistence.TemporalType;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+/**
+ * Controlador de ventas de clientes
+ *
+ *
+ */
+
 @Controller
 @RequestMapping("/ventas")
 public class SaleController {
@@ -44,29 +50,6 @@ public class SaleController {
         return result;
     }
 
-    /*@RequestMapping(value = "/informe", method = RequestMethod.POST)
-    public ModelAndView report(@ModelAttribute("date") String date) {
-        ModelAndView result = new ModelAndView("sale/report");
-        int day= Integer.parseInt(date.substring(8,10));
-        int month=Integer.parseInt(date.substring(5,7));
-        int year=Integer.parseInt(date.substring(0,4));
-
-        Calendar cal=new GregorianCalendar(year,month-1,day);
-        result.addObject("date",cal);
-        result.addObject("day",day);
-        result.addObject("month",month);
-        result.addObject("year",year);
-        List<PurchaseLine> lines=this.purchaseLineService.getPurchasesByDate(cal);
-        Set<User> user=new HashSet<>();
-        for (PurchaseLine l:lines
-        ) {
-            user.add(l.getUser());
-
-        }
-        result.addObject("users",user);
-        //  result.addObject("lines", this.purchaseLineService.getPurchasesByDate(cal));
-        return result;
-    }*/
 
 
     @GetMapping("/historial/{username}")
